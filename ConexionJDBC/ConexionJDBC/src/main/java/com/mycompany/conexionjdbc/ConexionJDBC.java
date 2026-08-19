ad577dfc41107e451efafe6ad4d2c235e8da19be
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-19 03:10:21 -0500

    inicio

---
+++ ConexionJDBC/ConexionJDBC/src/main/java/com/mycompany/conexionjdbc/ConexionJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conexionjdbc;
+
+/**
+ *
+ * @author juan
+ */
+public class ConexionJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
