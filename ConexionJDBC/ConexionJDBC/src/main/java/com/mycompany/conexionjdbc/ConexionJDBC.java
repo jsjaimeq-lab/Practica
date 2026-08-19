9a82711fe015503a23e606f775c31dbf09ef0c79
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-19 10:51:57 -0500

    guardado de como relizar conexion mysql JDBC

--- ConexionJDBC/ConexionJDBC/src/main/java/com/mycompany/conexionjdbc/ConexionJDBC.java
+++ ConexionJDBC/ConexionJDBC/src/main/java/com/mycompany/conexionjdbc/ConexionJDBC.java
@@ -1,16 +1,33 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
 package com.mycompany.conexionjdbc;
 
-/**
- *
- * @author juan
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
 public class ConexionJDBC {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+        String url = "jdbc:mysql://localhost:3306/user?useSSL=false&useTimezone=true&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+  
+        try{
+            Connection conexion = DriverManager.getConnection(url, "juan", "juan123");
+            Statement instrucion = conexion.createStatement();
+            String sql = "SELECT * FROM person";
+            ResultSet resultado = instrucion.executeQuery(sql);
+            
+            while(resultado.next()){
+                System.out.println(" name: "+resultado.getString("name"));
+            }
+        }catch(SQLException e){
+            e.printStackTrace(System.out);
+        }
     }
 }
