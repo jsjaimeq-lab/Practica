e14e6522a064db37a83900a8798acb24ff03703e
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-19 03:17:02 -0500

    yolo

--- ConexionJDBC/ConexionJDBC/src/main/java/com/mycompany/conexionjdbc/ConexionJDBC.java
+++ ConexionJDBC/ConexionJDBC/src/main/java/com/mycompany/conexionjdbc/ConexionJDBC.java
@@ -13,5 +13,10 @@ public class ConexionJDBC {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
+        
+        
+        
+        
+        
     }
 }
